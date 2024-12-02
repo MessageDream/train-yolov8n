5150e201d5b7255bc562ecf1d0b18abf2de04799
jayden <>
2024-12-02 06:33:58 +0000

    onnx

--- train.py
+++ train.py
@@ -1,11 +1,11 @@
+from os import path
 import yaml
 import tempfile
 
 from clearml import Task,Dataset,OutputModel
 
 
-task = Task.get_task(project_name='DevOps', task_name='Yolo8n Remote Training')
-output_model = OutputModel(task=task, framework="PyTorch")
+task = Task.current_task()
 params = task.get_parameters()
 model_variant = params["Args/model_variant"]
 tain_ds_id = params["Args/ds_id"]
@@ -36,17 +36,25 @@ from ultralytics import YOLO
 model = YOLO(f"{model_variant}.yaml")  # build a new model from scratch
 
 results = model.train(**args)
+print("Training completed results:", results)
+
+# # 提取权重文件路径
+# weights_dir = results.save_dir / 'weights'
+# best_weights = weights_dir / 'best.pt'
+# last_weights = weights_dir / 'last.pt'
 
 # 上传训练完成的 PyTorch 模型
-output_model.update_weights(weights_filename=f"{model_variant}.pt")
+output_model = OutputModel(task=task,name=f"{task.name}-pt", comment="PyTorch", framework="PyTorch")
+output_model.update_weights(weights_filename='best.pt')
 # output_model.publish()  # 可选：将 PyTorch 模型发布
 
 # 转换模型为 ONNX 格式
-onnx_name = f"{model_variant}.onnx"
 onnx_path = model.export(format="onnx", dynamic=True, opset=16)  # 导出 ONNX 模型
+print(f"ONNX model exported to {onnx_path}")
+onnx_name = path.basename(onnx_path)
 
 # 上传 ONNX 模型到 ClearML
-output_model_onnx = OutputModel(task=task, framework="ONNX")
+output_model_onnx = OutputModel(task=task,name=f"{task.name}_onnx",comment="ONNX", framework="ONNX")
 output_model_onnx.update_weights(weights_filename=onnx_name)
 # output_model_onnx.publish()  # 可选：将 ONNX 模型发布
 
