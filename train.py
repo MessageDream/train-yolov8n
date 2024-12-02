6710c659897e92883b48a3592177f7d41e685ca5
jayden <>
2024-12-02 07:12:36 +0000

    onnx path

--- train.py
+++ train.py
@@ -43,16 +43,21 @@ print("Training completed results:", results)
 # best_weights = weights_dir / 'best.pt'
 # last_weights = weights_dir / 'last.pt'
 
+local_dir = "runs/detect/train/weights/"
+model_file_name = "best"
+
 # 上传训练完成的 PyTorch 模型
 output_model = OutputModel(task=task,name=f"{task.name}-pt", comment="PyTorch", framework="PyTorch")
-uploaded_model_uri = output_model.update_weights(weights_filename='best.pt')
+uploaded_model_uri = output_model.update_weights(weights_filename=f'{model_file_name}.pt')
 print(f"PyTorch model uploaded to {uploaded_model_uri}")
 # output_model.publish()  # 可选：将 PyTorch 模型发布
 
 # 转换模型为 ONNX 格式
-onnx_path = model.export(format="onnx", dynamic=True, opset=16)  # 导出 ONNX 模型
+model.export(format="onnx", dynamic=True, opset=16)  # 导出 ONNX 模型
+onnx_name = f"{model_file_name}.onnx"
+onnx_path = path.join(local_dir, onnx_name)
+
 print(f"ONNX model exported to {onnx_path}")
-onnx_name = path.basename(onnx_path)
 
 # 上传 ONNX 模型到 ClearML
 output_model_onnx = OutputModel(task=task,name=f"{task.name}_onnx",comment="ONNX", framework="ONNX")
