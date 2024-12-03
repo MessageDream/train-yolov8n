831b87cc33345ef6d22fcbeeda5643bd173a16d1
jayden <>
2024-12-03 02:07:33 +0000

    model upload

--- train.py
+++ train.py
@@ -46,22 +46,26 @@ print("Training completed results:", results)
 local_dir = "runs/detect/train/weights/"
 model_file_name = "best"
 
+uploaded_model_uri = task.update_output_model(model_path=f"{local_dir}{model_file_name}.pt", model_name=f"{task.name}-pt")
+
 # # 上传训练完成的 PyTorch 模型
 # output_model = OutputModel(task=task,name=f"{task.name}-pt", comment="PyTorch", framework="PyTorch")
 # uploaded_model_uri = output_model.update_weights(weights_filename=f'{model_file_name}.pt')
-# print(f"PyTorch model uploaded to {uploaded_model_uri}")
-# # output_model.publish()  # 可选：将 PyTorch 模型发布
+print(f"PyTorch model uploaded to {uploaded_model_uri}")
+# output_model.publish()  # 可选：将 PyTorch 模型发布
 
 # 转换模型为 ONNX 格式
 model.export(format="onnx", dynamic=True, opset=16)  # 导出 ONNX 模型
 onnx_name = f"{model_file_name}.onnx"
-onnx_path = path.join(local_dir, onnx_name)
+script_dir = path.dirname(path.abspath(__file__))
+onnx_path = path.join(script_dir, onnx_name)
 
-print(f"ONNX model exported to {onnx_path}")
+# print(f"ONNX model exported to {onnx_path}")
 
 # 上传 ONNX 模型到 ClearML
-output_model_onnx = OutputModel(task=task,name=f"{task.name}-onnx",comment="ONNX", framework="ONNX")
-uploaded_onnx_model_uri = output_model_onnx.update_weights(weights_filename=onnx_name)
+uploaded_onnx_model_uri = task.update_output_model(model_path=onnx_path, model_name=f"{task.name}-onnx")
+# output_model_onnx = OutputModel(task=task,name=f"{task.name}-onnx",comment="ONNX", framework="ONNX")
+# uploaded_onnx_model_uri = output_model_onnx.update_weights(weights_filename=onnx_name)
 print(f"ONNX model uploaded to {uploaded_onnx_model_uri}")
 # output_model_onnx.publish()  # 可选：将 ONNX 模型发布
 
