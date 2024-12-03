4c07007c48c60467fa7b62a8d225cd66208895fc
jayden <>
2024-12-03 07:03:20 +0000

    upload test

--- train.py
+++ train.py
@@ -46,21 +46,30 @@ print("Training completed results:", results)
 local_dir = "runs/detect/train/weights/"
 model_file_name = "best"
 
-print(f"storage_uri:{task.storage_uri}")
+# upload_storage_uri
+output_models = task.models["output"]
+best_model = [m for m in output_models if m.name == f"{task.name}-best"][0]
 
-uploaded_model_uri = task.update_output_model(model_path=f"{local_dir}{model_file_name}.pt", model_name=f"{task.name}-pt")
+uploaded_model_uri = best_model.upload_storage_uri;
+print(f"PyTorch model uploaded to {uploaded_model_uri}")
+
+# uploaded_model_uri = task.update_output_model(model_path=f"{local_dir}{model_file_name}.pt", model_name=f"{task.name}-pt")
 
 # # 上传训练完成的 PyTorch 模型
 # output_model = OutputModel(task=task,name=f"{task.name}-pt", comment="PyTorch", framework="PyTorch")
 # uploaded_model_uri = output_model.update_weights(weights_filename=f'{model_file_name}.pt')
-print(f"PyTorch model uploaded to {uploaded_model_uri}")
+# print(f"PyTorch model uploaded to {uploaded_model_uri}")
 # output_model.publish()  # 可选：将 PyTorch 模型发布
+
+
 
 # 转换模型为 ONNX 格式
 model.export(format="onnx", dynamic=True, opset=16)  # 导出 ONNX 模型
 onnx_name = f"{model_file_name}.onnx"
 # script_dir = path.dirname(path.abspath(__file__))
 onnx_path = path.join(local_dir, onnx_name)
+remote_dir_path = uploaded_model_uri.rsplit('/', 1)[0]  # 移除最后的文件部分
+task.storage_uri = f"{remote_dir_path}/{onnx_name}"
 
 # print(f"ONNX model exported to {onnx_path}")
 
